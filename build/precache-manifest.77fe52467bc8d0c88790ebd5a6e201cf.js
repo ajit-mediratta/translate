bb7f564bedb1e60c58a9938446c7503a0535c791
ajit-mediratta <ajit.mediratta@gmail.com>
2019-05-31 11:21:44 +0530

    Initial commit

---
+++ build/precache-manifest.77fe52467bc8d0c88790ebd5a6e201cf.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "51481fe428b7edd84335",
+    "url": "/static/js/main.51481fe4.chunk.js"
+  },
+  {
+    "revision": "5cb9b55e9859431b45ee",
+    "url": "/static/js/1.5cb9b55e.chunk.js"
+  },
+  {
+    "revision": "7c7a7ff0b4e86c090171c0a9496c743c",
+    "url": "/index.html"
+  }
+];
